5164aeb64cea6b010cc29feb9c3d7310dcb4100a
Lorenzo Bucci <lorenzo.bucci@stud.unifi.it>
2021-01-22 18:11:19 +0100

    Revert latches implementation and add division of the main matrix into submatrices Final implementation with a good speedup

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 import java.lang.instrument.Instrumentation;
 import java.nio.charset.StandardCharsets;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
 
 public class Main {
 
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
+    public static void main(String[] args) throws InterruptedException {
 
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
